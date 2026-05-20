0a5d6fa626b635251231064c8cffa6b5ad737275
brightsideplayers <morna.scott.dunne@gmail.com>
2026-05-20 17:33:36 -0400

    Create constants.ts

---
+++ src/data/constants.ts
@@ -0,0 +1,34 @@
+
+export const predefinedSkills = [
+  "Actors",
+  "Costume",
+  "Directing",
+  "Executive",
+  "Photography",
+  "Props",
+  "Set Design",
+  "Social Media",
+  "Stage Crew"
+];
+
+export const teamMembers = [
+  "Jenn Bryant",
+  "Cathy Kates",
+  "Morna Scott-Dunne",
+  "Lesley Quinn"
+];
+
+export const hashtagPresets = {
+  "Funny Panto": [
+    "#Pantomime",
+    "#ComedyTheatre",
+    "#FunnyScenes",
+    "#TheatreTok"
+  ],
+  "Local Milton": [
+    "#MiltonON",
+    "#MiltonOntario",
+    "#HaltonRegion",
+    "#OntarioTheatre"
+  ]
+};
