aa735904670824a9101a82c803033b1fc7070a76
brightsideplayers <morna.scott.dunne@gmail.com>
2026-05-21 17:36:03 -0400

    Update publishScheduledPosts.js

--- api/publishScheduledPosts.js
+++ api/publishScheduledPosts.js
@@ -1,1 +1,14 @@
+export default async function handler(
+  req,
+  res
+) {
+  console.log(
+    "Cron job triggered"
+  );
 
+  return res.status(200).json({
+    success: true,
+    message:
+      "Scheduled publisher running"
+  });
+}
