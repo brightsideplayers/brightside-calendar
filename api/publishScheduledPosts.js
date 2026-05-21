e28049f91023e5c1acd7cf474aa8c134c496d2a8
brightsideplayers <morna.scott.dunne@gmail.com>
2026-05-21 18:41:19 -0400

    Update publishScheduledPosts.js

--- api/publishScheduledPosts.js
+++ api/publishScheduledPosts.js
@@ -1,8 +1,25 @@
-export default function handler(
+import { db } from "../firebaseAdmin";
+
+export default async function handler(
   req,
   res
 ) {
+  const snapshot =
+    await db
+      .collection("posts")
+      .get();
+
+  const posts = snapshot.docs.map(
+    (doc) => ({
+      id: doc.id,
+      ...doc.data()
+    })
+  );
+
+  console.log(posts);
+
   return res.status(200).json({
-    success: true
+    success: true,
+    posts
   });
 }
