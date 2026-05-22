c00e4d32dd00eb9214e3bf2bd49a7a32002099fd
brightsideplayers <morna.scott.dunne@gmail.com>
2026-05-22 13:11:52 -0400

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,23 +1,22 @@
 import admin from "firebase-admin";
 
 const privateKey =
-  process.env.FIREBASE_PRIVATE_KEY
-    ?.replace(/\\n/g, "\n");
+  process.env.FIREBASE_PRIVATE_KEY?.replace(
+    /\\n/g,
+    "\n"
+  );
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential:
-      admin.credential.cert({
-        projectId:
-          process.env
-            .FIREBASE_PROJECT_ID,
+    credential: admin.credential.cert({
+      projectId:
+        process.env.FIREBASE_PROJECT_ID,
 
-        clientEmail:
-          process.env
-            .FIREBASE_CLIENT_EMAIL,
+      clientEmail:
+        process.env.FIREBASE_CLIENT_EMAIL,
 
-        privateKey
-      })
+      privateKey
+    })
   });
 }
 
