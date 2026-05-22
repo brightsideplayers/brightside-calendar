d0bbe02e5aeb3acb2221a2f9142124ce14a4a8a1
brightsideplayers <morna.scott.dunne@gmail.com>
2026-05-22 14:56:39 -0400

    Update publishScheduledPosts.js

--- api/publishScheduledPosts.js
+++ api/publishScheduledPosts.js
@@ -42,41 +42,47 @@ export default async function handler(
         continue;
       }
 
+      // FACEBOOK
       if (post.platform === "Facebook") {
-  const facebookRes =
-    await fetch(
-      `https://graph.facebook.com/v23.0/${process.env.FACEBOOK_PAGE_ID}/photos`,
-      {
-        method: "POST",
+        const facebookRes =
+          await fetch(
+            `https://graph.facebook.com/v23.0/${process.env.FACEBOOK_PAGE_ID}/photos`,
+            {
+              method: "POST",
 
-        headers: {
-          "Content-Type":
-            "application/json"
-        },
+              headers: {
+                "Content-Type":
+                  "application/json"
+              },
 
-        body: JSON.stringify({
-          url: post.imageUrl,
+              body: JSON.stringify({
+                url: post.imageUrl,
 
-          caption:
-            post.caption,
+                caption:
+                  post.caption,
 
-          access_token:
-            process.env
-              .INSTAGRAM_ACCESS_TOKEN
-        })
+                access_token:
+                  process.env
+                    .INSTAGRAM_ACCESS_TOKEN
+              })
+            }
+          );
+
+        const facebookData =
+          await facebookRes.json();
+
+        results.push({
+          postId: post.id,
+          facebookData
+        });
+
+        continue;
       }
-    );
 
-  const facebookData =
-    await facebookRes.json();
-
-  results.push({
-    postId: post.id,
-    facebookData
-  });
-
-  continue;
-}
+      // INSTAGRAM
+      const createMediaRes =
+        await fetch(
+          `https://graph.facebook.com/v23.0/${process.env.INSTAGRAM_USER_ID}/media`,
           {
             method: "POST",
 
@@ -100,45 +106,45 @@ export default async function handler(
         );
 
       const mediaData =
-  await createMediaRes.json();
+        await createMediaRes.json();
 
-if (mediaData.id) {
-  const publishRes =
-    await fetch(
-      `https://graph.facebook.com/v23.0/${process.env.INSTAGRAM_USER_ID}/media_publish`,
-      {
-        method: "POST",
+      if (mediaData.id) {
+        const publishRes =
+          await fetch(
+            `https://graph.facebook.com/v23.0/${process.env.INSTAGRAM_USER_ID}/media_publish`,
+            {
+              method: "POST",
 
-        headers: {
-          "Content-Type":
-            "application/json"
-        },
+              headers: {
+                "Content-Type":
+                  "application/json"
+              },
 
-        body: JSON.stringify({
-          creation_id:
-            mediaData.id,
+              body: JSON.stringify({
+                creation_id:
+                  mediaData.id,
 
-          access_token:
-            process.env
-              .INSTAGRAM_ACCESS_TOKEN
-        })
+                access_token:
+                  process.env
+                    .INSTAGRAM_ACCESS_TOKEN
+              })
+            }
+          );
+
+        const publishData =
+          await publishRes.json();
+
+        results.push({
+          postId: post.id,
+          mediaData,
+          publishData
+        });
+      } else {
+        results.push({
+          postId: post.id,
+          mediaData
+        });
       }
-    );
-
-  const publishData =
-    await publishRes.json();
-
-  results.push({
-    postId: post.id,
-    mediaData,
-    publishData
-  });
-} else {
-  results.push({
-    postId: post.id,
-    mediaData
-  });
-}
     }
 
     return res.status(200).json({
