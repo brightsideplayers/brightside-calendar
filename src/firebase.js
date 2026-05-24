110993a747d429bea9f5f008a724c830ba7f9db6
brightsideplayers <morna.scott.dunne@gmail.com>
2026-05-24 19:15:53 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,38 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+
+import {
+  getFirestore
+} from "firebase/firestore";
+
+import {
+  getAuth,
+  GoogleAuthProvider
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAZ3nQpzf246EepmNDK6Qs32q-G4-b8R4E",
-  authDomain: "brightside-calendar-50da5.firebaseapp.com",
-  projectId: "brightside-calendar-50da5",
-  storageBucket: "brightside-calendar-50da5.firebasestorage.app",
-  messagingSenderId: "342676365953",
-  appId: "1:342676365953:web:d4fb633c1bb0d8af09e380"
+
+  authDomain:
+    "brightside-calendar-50da5.firebaseapp.com",
+
+  projectId:
+    "brightside-calendar-50da5",
+
+  storageBucket:
+    "brightside-calendar-50da5.firebasestorage.app",
+
+  messagingSenderId:
+    "342676365953",
+
+  appId:
+    "1:342676365953:web:d4fb633c1bb0d8af09e380"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
+export const auth = getAuth(app);
+
+export const provider =
+  new GoogleAuthProvider();
